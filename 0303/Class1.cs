e6a056a4a5f467dfd96eb8b5ea5efccfeb89c2b0
plus <1102137105@cc.kuas.edu.tw>
2016-03-03 15:00:21 +0800

    helloword

---
+++ 0303/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _0303
+{
+    public class Class1
+    {
+        public static void main() {
+            System.Console.WriteLine("helloword");
+            System.Console.ReadLine();
+        }
+    }
+}
